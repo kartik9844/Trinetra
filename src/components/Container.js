f4780aae74dc1224c9c4d83be38b3c98d208ec6b
P Sandeep Rao <142883216+srao116@users.noreply.github.com>
2023-09-05 15:37:23 +0530

    aro3

--- src/components/Container.js
+++ src/components/Container.js
@@ -9,12 +9,14 @@ const Container = () => {
           </span>
           <span className="uppercase text-red">*</span>
         </b>
-        <div className="rounded-23xl box-border w-[574.37px] h-[65.08px] flex flex-row p-2.5 items-center justify-center text-xl font-fresca border-[2px] border-solid border-black">
-          <div className="relative inline-block w-[220px] shrink-0">
-            <span className="uppercase">e</span>
-            <span className="lowercase">nter your mail address</span>
-          </div>
-        </div>
+        <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  placeholder={"enter your email"}
+                  className="rounded-6xl box-border w-[550px] h-[65px] flex flex-row p-1.5 items-center justify-center text-xl text-black font-fresca border-[2px] border-solid border-black"
+                />  
       </div>
       <div className="w-[565.84px] h-[113.14196014404297px] flex flex-col py-0 pr-0 pl-[0.000008140923455357552px] box-border items-start justify-start gap-[9px] ml-[9px]">
         <b className="inline-block w-[206.73px] h-[44.055274963378906px] shrink-0 ml-[17px]">
@@ -26,9 +28,14 @@ const Container = () => {
             <span className="text-red">*</span>
           </span>
         </b>
-        <div className="rounded-23xl box-border w-[565.84px] h-[60.07537841796875px] flex flex-row p-2.5 items-center justify-center ml-0 text-xl font-fresca border-[2px] border-solid border-black">
-          <div className="relative capitalize">{`Enter password                                                               `}</div>
-        </div>
+        <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  autoComplete="password"
+                  placeholder={"enter your password"}
+                  className="rounded-6xl box-border w-[550px] h-[65px] flex flex-row p-1.5 items-center justify-center text-xl text-black font-fresca border-[2px] border-solid border-black"
+                /> 
       </div>
     </div>
   );
