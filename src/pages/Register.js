32c984303c9daf5b9a6650d21f23f505daedae87
kartik9844 <96242043+kartik9844@users.noreply.github.com>
2023-09-07 12:41:34 +0530

    joshi2

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HeaderTop1 from "../components/HeaderTop1";
 import StartupForm from '../components/startf';
 
 function Register() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Startup Registration Form</h1>
+      <HeaderTop1 />
+      <h1 className="text-2xl font-bold mb-4 text-center justify-self-center">Startup Registration Form</h1>
       <StartupForm />
     </div>
   );
