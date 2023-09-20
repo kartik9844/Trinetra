03d5092eea0f58ccb1666fd8337205b4c0cec431
nikita rokhade <rokhadenikita@gmail.com>
2023-09-20 12:29:14 +0530

    ni

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -133,7 +133,7 @@ const AddProduct = () => {
                   id="country"
                   name="country"
                   autoComplete="country-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 >
                   <option>United States</option>
                   <option>Canada</option>
